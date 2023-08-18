278b8dcf5ff750b6e3f4fd635a9ac94b41ad15e2
LetifeEynullayeva <121341394+LetifeEynullayeva@users.noreply.github.com>
2023-08-18 14:44:46 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,224 @@
+var arr=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','_']
+
+$('.a').on('click',function(){
+            var herf=['a']
+            arr.push(herf)
+            var YenIp=document.createElement('p')
+            YenIp.setAttribute('class','box')
+            YenIp.innerText=herf;
+            $('.write').append(YenIp)
+        })
+
+        $('.b').on('click',function(){
+            var herf=['b ']
+            arr.push(herf)
+            var YenIp=document.createElement('p')
+            YenIp.setAttribute('class','box')
+            YenIp.innerText=herf;
+            $('.write').append(YenIp)
+        })
+        $('.c').on('click',function(){
+            var herf=['c']
+            arr.push(herf)
+            var YenIp=document.createElement('p')
+            YenIp.setAttribute('class','box')
+            YenIp.innerText=herf;
+            $('.write').append(YenIp)
+        })
+        $('.d').on('click',function(){
+            var herf=['d']
+            arr.push(herf)
+            var YenIp=document.createElement('p')
+            YenIp.setAttribute('class','box')
+            YenIp.innerText=herf;
+            $('.write').append(YenIp)
+        })
+        $('.e').on('click',function(){
+            var herf=['e']
+            arr.push(herf)
+            var YenIp=document.createElement('p')
+            YenIp.setAttribute('class','box')
+            YenIp.innerText=herf;
+            $('.write').append(YenIp)
+        })
+
+        $('.f').on('click',function(){
+            var herf=['f']
+            arr.push(herf)
+            var YenIp=document.createElement('p')
+            YenIp.setAttribute('class','box')
+            YenIp.innerText=herf;
+            $('.write').append(YenIp)
+        })
+        $('.g').on('click',function(){
+            var herf=['g']
+            arr.push(herf)
+            var YenIp=document.createElement('p')
+            YenIp.setAttribute('class','box')
+            YenIp.innerText=herf;
+            $('.write').append(YenIp)
+        })
+        $('.h').on('click',function(){
+            var herf=['h']
+            arr.push(herf)
+            var YenIp=document.createElement('p')
+            YenIp.setAttribute('class','box')
+            YenIp.innerText=herf;
+            $('.write').append(YenIp)
+        })
+
+        $('.i').on('click',function(){
+            var herf=['i']
+            arr.push(herf)
+            var YenIp=document.createElement('p')
+            YenIp.setAttribute('class','box')
+            YenIp.innerText=herf;
+            $('.write').append(YenIp)
+        })
+        $('.j').on('click',function(){
+            var herf=['j']
+            arr.push(herf)
+            var YenIp=document.createElement('p')
+            YenIp.setAttribute('class','box')
+            YenIp.innerText=herf;
+            $('.write').append(YenIp)
+        })
+        $('.k').on('click',function(){
+            var herf=['k']
+            arr.push(herf)
+            var YenIp=document.createElement('p')
+            YenIp.setAttribute('class','box')
+            YenIp.innerText=herf;
+            $('.write').append(YenIp)
+        })
+        $('.l').on('click',function(){
+            var herf=['l']
+            arr.push(herf)
+            var YenIp=document.createElement('p')
+            YenIp.setAttribute('class','box')
+            YenIp.innerText=herf;
+            $('.write').append(YenIp)
+        })
+        $('.m').on('click',function(){
+            var herf=['m']
+            arr.push(herf)
+            var YenIp=document.createElement('p')
+            YenIp.setAttribute('class','box')
+            YenIp.innerText=herf;
+            $('.write').append(YenIp)
+        })
+        $('.n').on('click',function(){
+            var herf=['n']
+            arr.push(herf)
+            var YenIp=document.createElement('p')
+            YenIp.setAttribute('class','box')
+            YenIp.innerText=herf;
+            $('.write').append(YenIp)
+        })
+        $('.o').on('click',function(){
+            var herf=['o']
+            arr.push(herf)
+            var YenIp=document.createElement('p')
+            YenIp.setAttribute('class','box')
+            YenIp.innerText=herf;
+            $('.write').append(YenIp)
+        })
+        $('.p').on('click',function(){
+            var herf=['p']
+            arr.push(herf)
+            var YenIp=document.createElement('p')
+            YenIp.setAttribute('class','box')
+            YenIp.innerText=herf;
+            $('.write').append(YenIp)
+        })
+        $('.q').on('click',function(){
+            var herf=['q']
+            arr.push(herf)
+            var YenIp=document.createElement('p')
+            YenIp.setAttribute('class','box')
+            YenIp.innerText=herf;
+            $('.write').append(YenIp)
+        })
+        $('.r').on('click',function(){
+            var herf=['r']
+            arr.push(herf)
+            var YenIp=document.createElement('p')
+            YenIp.setAttribute('class','box')
+            YenIp.innerText=herf;
+            $('.write').append(YenIp)
+        })
+        $('.s').on('click',function(){
+            var herf=['s']
+            arr.push(herf)
+            var YenIp=document.createElement('p')
+            YenIp.setAttribute('class','box')
+            YenIp.innerText=herf;
+            $('.write').append(YenIp)
+        })
+        $('.t').on('click',function(){
+            var herf=['t']
+            arr.push(herf)
+            var YenIp=document.createElement('p')
+            YenIp.setAttribute('class','box')
+            YenIp.innerText=herf;
+            $('.write').append(YenIp)
+        })
+        $('.u').on('click',function(){
+            var herf=['u']
+            arr.push(herf)
+            var YenIp=document.createElement('p')
+            YenIp.setAttribute('class','box')
+            YenIp.innerText=herf;
+            $('.write').append(YenIp)
+        })
+        $('.v').on('click',function(){
+            var herf=['v']
+            arr.push(herf)
+            var YenIp=document.createElement('p')
+            YenIp.setAttribute('class','box')
+            YenIp.innerText=herf;
+            $('.write').append(YenIp)
+        })
+        $('.w').on('click',function(){
+            var herf=['w']
+            arr.push(herf)
+            var YenIp=document.createElement('p')
+            YenIp.setAttribute('class','box')
+            YenIp.innerText=herf;
+            $('.write').append(YenIp)
+        })
+        $('.x').on('click',function(){
+            var herf=['x']
+            arr.push(herf)
+            var YenIp=document.createElement('p')
+            YenIp.setAttribute('class','box')
+            YenIp.innerText=herf;
+            $('.write').append(YenIp)
+        })
+        $('.y').on('click',function(){
+            var herf=['y']
+            arr.push(herf)
+            var YenIp=document.createElement('p')
+            YenIp.setAttribute('class','box')
+            YenIp.innerText=herf;
+            $('.write').append(YenIp)
+        })
+        $('.z').on('click',function(){
+            var herf=['z']
+            arr.push(herf)
+            var YenIp=document.createElement('p')
+            YenIp.setAttribute('class','box')
+            YenIp.innerText=herf;
+            $('.write').append(YenIp)
+        })
+        $('._').on('click',function(){
+            var herf=['_']
+            arr.push(herf)
+            var YenIp=document.createElement('p')
+            YenIp.setAttribute('class','box')
+            YenIp.innerText=herf;
+            $('.write').append(YenIp)
+        })
+        $('.clear').on('click',function(){
+            $('.write p').hide()
+        })
